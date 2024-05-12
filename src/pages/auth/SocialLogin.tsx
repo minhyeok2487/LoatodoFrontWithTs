8fbd7e043545dfa4754c2d41f5889a777c293a5c
Minhyeok1995 <repeater2487@naver.com>
2024-05-12 10:23:02 +0900

    소셜 로그인 성공시 react query 갱신

--- src/pages/auth/SocialLogin.tsx
+++ src/pages/auth/SocialLogin.tsx
@@ -1,4 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
+import { useMember } from "../../core/apis/Member.api";
+import { useCharacters } from "../../core/apis/Character.api";
+import { useFriends } from "../../core/apis/Friend.api";
 
 const SocialLogin = () => {
   const location = useLocation();
@@ -11,8 +14,16 @@ const SocialLogin = () => {
 
   const token = getUrlParameter("token");
 
+  const { data: member, refetch: refetchMember } = useMember();
+  const { data: characters, refetch: refetchCharacters } = useCharacters();
+  const { data: friends, refetch: refetchFriends } = useFriends();
+
+
   if (token) {
     localStorage.setItem("ACCESS_TOKEN", token);
+    refetchMember();
+    refetchCharacters();
+    refetchFriends();
     return (
       <Navigate
         to={{
