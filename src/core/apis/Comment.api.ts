e6ad23dd3d6c329c67fd61957250f6451733b284
Minhyeok1995 <repeater2487@naver.com>
2024-06-01 07:03:14 +0900

    방명록 수정 삭제 기능 추가

--- src/core/apis/Comment.api.ts
+++ src/core/apis/Comment.api.ts
@@ -1,14 +1,37 @@
 import { CommentsType } from "../types/Comment.type.";
 import api from "./api";
 
-export async function getComments(page:number): Promise<CommentsType> {
+export async function getComments(page: number): Promise<CommentsType> {
   return await api.get(`/v3/comments?page=${page}`).then((res) => res.data);
 }
 
-export async function addComment(text:string, parentId?:number): Promise<CommentsType[]> {
+export async function addComment(
+  text: string,
+  parentId?: number
+): Promise<CommentsType[]> {
   const updateContent = {
     body: text,
-    parentId: parentId
-  }
+    parentId: parentId,
+  };
   return await api.post("/v3/comments", updateContent).then((res) => res.data);
+}
+
+export async function updateComment(
+  text: string,
+  commentId: number,
+  page: number
+): Promise<any> {
+  const updateContent = {
+    body: text,
+    id: commentId,
+  };
+  return await api
+    .patch(`/v3/comments?page=${page}`, updateContent)
+    .then((res) => res.data);
+}
+
+export async function deleteComment(commentId: number): Promise<any> {
+  return await api
+    .delete(`/v3/comments/${commentId}`)
+    .then((res) => res.data);
 }
