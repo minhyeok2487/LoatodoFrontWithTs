7fd2a10b3bff0e950ef9f6a177958485154b10d1
jhyoon97 <jecgdd@naver.com>
2024-06-12 14:44:56 +0900

    * 폰트와 이미지들 pulic 폴더 대신 src/assets 폴더에서 관리 * 절대경로 + alias import 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomeIndex from "./pages/home/HomeIndex";
-import TodoIndex from "./pages/todo/TodoIndex";
-import Login from "./pages/auth/Login";
-import SocialLogin from "./pages/auth/SocialLogin";
-import Logout from "./pages/auth/Logout";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { serverState } from "./core/atoms/Todo.atom";
-import { useCharacters } from "./core/apis/Character.api";
-import { useMember } from "./core/apis/Member.api";
 import { useEffect } from "react";
-import { getDefaultServer } from "./core/func/todo.fun";
-import FriendsIndex from "./pages/friends/FriendsIndex";
-import FriendTodo from "./pages/friends/FriendTodo";
-import CharacterSetting from "./components/CharacterSetting";
-import CommentsIndex from "./pages/comments/CommentsIndex";
-import Board from "./pages/boards/Board";
-import BoardInsertForm from "./pages/boards/BoardInsertForm";
-import ApiKeyUpdateForm from "./pages/member/ApiKeyUpdateForm";
-import { ThemeEnums } from "./core/enum/ThemeEnum";
-import { themeMode } from "./core/atoms/Theme.atom";
-import SignUp from "./pages/auth/SignUp";
-import SignUpCharacters from "./pages/auth/SignUpCharacters";
-import TodoAllIndex from "./pages/todo/TodoAllIndex";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useRecoilState, useRecoilValue } from "recoil";
 
-function App() {
+import Login from "@pages/auth/Login";
+import Logout from "@pages/auth/Logout";
+import SignUp from "@pages/auth/SignUp";
+import SignUpCharacters from "@pages/auth/SignUpCharacters";
+import SocialLogin from "@pages/auth/SocialLogin";
+import Board from "@pages/boards/Board";
+import BoardInsertForm from "@pages/boards/BoardInsertForm";
+import CommentsIndex from "@pages/comments/CommentsIndex";
+import FriendTodo from "@pages/friends/FriendTodo";
+import FriendsIndex from "@pages/friends/FriendsIndex";
+import HomeIndex from "@pages/home/HomeIndex";
+import ApiKeyUpdateForm from "@pages/member/ApiKeyUpdateForm";
+import TodoAllIndex from "@pages/todo/TodoAllIndex";
+import TodoIndex from "@pages/todo/TodoIndex";
+
+import { useCharacters } from "@core/apis/Character.api";
+import { useMember } from "@core/apis/Member.api";
+import { themeMode } from "@core/atoms/Theme.atom";
+import { serverState } from "@core/atoms/Todo.atom";
+import { ThemeEnums } from "@core/enum/ThemeEnum";
+import { getDefaultServer } from "@core/func/todo.fun";
+
+import CharacterSetting from "@components/CharacterSetting";
+
+const App = () => {
   const [server, setServer] = useRecoilState(serverState);
   const { data: characters } = useCharacters();
   const { data: member } = useMember();
@@ -52,7 +55,7 @@ function App() {
           <Route path="" element={<HomeIndex />} />
 
           {/* 로그인 관련 */}
-          <Route path="/login" element={<Login message={""} />} />
+          <Route path="/login" element={<Login message="" />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/sociallogin" element={<SocialLogin />} />
           <Route path="/signup" element={<SignUp />} />
@@ -78,6 +81,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 import { createRoot, hydrateRoot } from "react-dom/client";
 
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
@@ -22,8 +22,7 @@ const element = (
   </React.StrictMode>
 );
 
-
-if (rootElement.hasChildNodes()){
+if (rootElement.hasChildNodes()) {
   hydrateRoot(rootElement, element);
 } else {
   const root = createRoot(rootElement);
