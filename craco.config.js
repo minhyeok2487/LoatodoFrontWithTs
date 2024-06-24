5c048e6d5c072dd08313c1939b4d5a551b115936
jhyoon97 <jecgdd@naver.com>
2024-06-24 22:01:21 +0900

    빌드 시 analyzer가 열리지 않도록 수정했으며 서버로 실행하지 않고 html파일을 생성하도록 수정

--- craco.config.js
+++ craco.config.js
@@ -2,21 +2,28 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
-  webpack: {
-    plugins: [new BundleAnalyzerPlugin({ analyzerMode: "server" })],
-  },
-  babel: {
-    plugins: ["@emotion"],
-  },
-  plugins: [
-    {
-      plugin: {
-        overrideWebpackConfig: ({ webpackConfig }) => {
-          webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
-          return webpackConfig;
+module.exports = function () {
+  return {
+    webpack: {
+      plugins: [
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static",
+          openAnalyzer: false,
+        }),
+      ],
+    },
+    babel: {
+      plugins: ["@emotion"],
+    },
+    plugins: [
+      {
+        plugin: {
+          overrideWebpackConfig: ({ webpackConfig }) => {
+            webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+            return webpackConfig;
+          },
         },
       },
-    },
-  ],
+    ],
+  };
 };
