55942536711a35d0487939e95b7d255672ab1d49
jhyoon97 <jecgdd@naver.com>
2024-07-20 19:33:57 +0900

    theme 컬러값에 palette 적용

--- src/core/constants/palette.ts
+++ src/core/constants/palette.ts
@@ -1,4 +1,4 @@
-export const gray = {
+const gray = {
   0: "#ffffff",
   50: "#f0f0f0",
   100: "#e9e9e9",
@@ -19,23 +19,23 @@ export const gray = {
   1000: "#000000",
 };
 
-export const yellow = {
+const yellow = {
   200: "#f7d681",
   300: "#ffc74f",
   350: "#f3be43",
 };
 
-export const green = {
+const green = {
   50: "#e4f5d7",
   350: "#94e35d",
   600: "#00b62d",
 };
 
-export const smokeGreen = {
+const smokeGreen = {
   400: "#73a982",
 };
 
-export const red = {
+const red = {
   0: "#ffe2e2",
   50: "#ffd8d2",
   200: "#ff8d7e",
@@ -43,26 +43,38 @@ export const red = {
   450: "#ff0000",
 };
 
-export const smokeRed = {
+const smokeRed = {
   150: "#e9b4ac",
   300: "#e96161",
 };
 
-export const blue = {
+const blue = {
   0: "#e2f0ff",
   50: "#bbe4ff",
   350: "#2986ff",
   450: "#001dff",
 };
 
-export const lightBlue = {
+const lightBlue = {
   450: "#1ddfee",
 };
 
-export const smokeBlue = {
+const smokeBlue = {
   0: "#eff1f7",
   200: "#abc1cf",
   500: "#2c79bd",
+};
+
+export default {
+  gray,
+  yellow,
+  green,
+  smokeGreen,
+  red,
+  smokeRed,
+  blue,
+  lightBlue,
+  smokeBlue,
 };
 
 // https://www.w3schools.com/colors/colors_picker.asp
