7c9052d0ff35fce5bc86a2f53120b06dd3bda89b
jhyoon97 <jecgdd@naver.com>
2024-08-09 00:04:03 +0900

    캐릭터 메모 ui와 api 연동 완료

--- src/components/todo/TodolList/CharacterInformation.tsx
+++ src/components/todo/TodolList/CharacterInformation.tsx
@@ -1,39 +1,121 @@
+import { MdSave } from "@react-icons/all-files/md/MdSave";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRef, useState } from "react";
 import styled from "styled-components";
 
+import useUpdateCharacterMemo from "@core/hooks/mutations/character/useUpdateCharacterMemo";
 import type { Character } from "@core/types/character";
 import { getIsSpecialist } from "@core/utils/character.util";
+import queryKeyGenerator from "@core/utils/queryKeyGenerator";
+
+import PiNotePencil from "@assets/svg/PiNotePencil";
 
 interface Props {
   character: Character;
 }
 
 const CharacterInformation = ({ character }: Props) => {
-  return (
-    <Wrapper
-      style={{
-        backgroundImage:
-          character.characterImage !== null
-            ? `url(${character.characterImage})`
-            : undefined,
-        backgroundPosition: getIsSpecialist(character.characterClassName)
-          ? "left 10px top -80px"
-          : "left 10px top -30px",
-      }}
-    >
-      {character.goldCharacter && <GoldCharacter>골드 획득 지정</GoldCharacter>}
+  const queryClient = useQueryClient();
+  const memoRef = useRef<HTMLInputElement>(null);
 
-      <Server>
-        @{character.serverName} {character.characterClassName}
-      </Server>
-      <Nickname>{character.characterName}</Nickname>
-      <Level>Lv. {character.itemLevel}</Level>
+  const [editMemo, setEditMemo] = useState(false);
+
+  const updateCharacterMemo = useUpdateCharacterMemo({
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queryKeyGenerator.getCharacters(),
+      });
+
+      setEditMemo(false);
+    },
+  });
+
+  const submitMemo = () => {
+    if (memoRef.current) {
+      updateCharacterMemo.mutate({
+        characterId: character.characterId,
+        memo: memoRef.current.value,
+      });
+    }
+  };
+
+  return (
+    <Wrapper>
+      <CharacterBox
+        style={{
+          backgroundImage:
+            character.characterImage !== null
+              ? `url(${character.characterImage})`
+              : undefined,
+          backgroundPosition: getIsSpecialist(character.characterClassName)
+            ? "left 10px top -80px"
+            : "left 10px top -30px",
+        }}
+      >
+        {character.goldCharacter && <GoldBadge>골드 획득 지정</GoldBadge>}
+
+        <Server>
+          @{character.serverName} {character.characterClassName}
+        </Server>
+        <Nickname>{character.characterName}</Nickname>
+        <Level>Lv. {character.itemLevel}</Level>
+
+        <Buttons>
+          {editMemo && (
+            <button type="button" onClick={submitMemo}>
+              <MdSave />
+            </button>
+          )}
+          {!editMemo && (
+            <button
+              type="button"
+              onClick={() => {
+                setEditMemo(true);
+                memoRef.current?.focus();
+              }}
+            >
+              <PiNotePencil />
+            </button>
+          )}
+        </Buttons>
+      </CharacterBox>
+      <MemoInput
+        ref={memoRef}
+        placeholder="메모 추가"
+        defaultValue={character.memo || ""}
+        $isHidden={character.memo === null && !editMemo}
+        onKeyDown={(e) => {
+          e.stopPropagation();
+          const target = e.target as HTMLInputElement;
+
+          if (e.key === "Enter") {
+            submitMemo();
+
+            target.blur();
+          }
+        }}
+      />
     </Wrapper>
   );
 };
 
 export default CharacterInformation;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div``;
+
+const MemoInput = styled.input<{ $isHidden: boolean }>`
+  position: ${({ $isHidden }) => ($isHidden ? "absolute" : "relative")};
+  left: ${({ $isHidden }) => ($isHidden ? "-9999px" : "unset")};
+  padding: 5px 10px;
+  width: 100%;
+  font-size: 12px;
+  border: 1px solid ${({ theme }) => theme.app.border};
+  border-bottom: none;
+  background: ${({ theme }) => theme.app.bg.white};
+`;
+
+const CharacterBox = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -49,7 +131,7 @@ const Wrapper = styled.div`
   background-size: 150%;
 `;
 
-const GoldCharacter = styled.div`
+const GoldBadge = styled.div`
   padding: 2px 5px;
   margin-bottom: 8px;
   background: ${({ theme }) => theme.app.palette.yellow[350]};
@@ -72,4 +154,21 @@ const Nickname = styled.span`
 
 const Level = styled.span`
   font-size: 14px;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  position: absolute;
+  background: rgba(0, 0, 0, 0.8);
+  bottom: 0;
+  right: 0;
+  padding: 5px;
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
 `;
