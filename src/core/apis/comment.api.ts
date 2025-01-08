b8dea8bf991e45ee6f1dacc9dea02ba256f64779
Minhyeok1995 <repeater2487@naver.com>
2025-01-09 06:55:02 +0900

    refactor: Removed Comments(Legacy)

--- src/core/apis/comment.api.ts
+++ src/core/apis/comment.api.ts
@@ -1,7 +1,4 @@
-import type { NoDataResponse } from "@core/types/api";
 import type {
-  AddCommentRequest,
-  EditCommentRequest,
   GetCommentsRequest,
   GetCommentsResponse,
 } from "@core/types/comment";
@@ -12,34 +9,10 @@ export const getComments = ({
   page,
 }: GetCommentsRequest): Promise<GetCommentsResponse> => {
   return mainAxios
-    .get(`/v3/comments`, {
+    .get(`/api/v1/comments`, {
       params: {
         page,
       },
     })
     .then((res) => res.data);
-};
-
-export const addComment = ({
-  parentId,
-  body,
-}: AddCommentRequest): Promise<NoDataResponse> => {
-  return mainAxios.post("/v3/comments", {
-    parentId,
-    body,
-  });
-};
-
-export const editComment = ({
-  id,
-  body,
-}: EditCommentRequest): Promise<NoDataResponse> => {
-  return mainAxios.patch(`/v4/comments`, {
-    id,
-    body,
-  });
-};
-
-export const removeComment = (commentId: number): Promise<NoDataResponse> => {
-  return mainAxios.delete(`/v3/comments/${commentId}`);
 };
