2d3c50b84954804d6003d777652dd4369511767f
Minhyeok1995 <repeater2487@naver.com>
2025-02-24 11:03:39 +0900

    feat: Restore Logs menu item in Header navigation

--- src/layouts/common/Header/index.tsx
+++ src/layouts/common/Header/index.tsx
@@ -50,11 +50,10 @@ const leftMenues: Array<{
     to: "https://docs.loatodo.com",
     title: "가이드",
   },
-  // {
-  //   to: "/logs",
-  //   title: "로그",
-  //   span: true,
-  // },
+  {
+    to: "/logs",
+    title: "타임라인",
+  },
 ];
 
 const Header = () => {
